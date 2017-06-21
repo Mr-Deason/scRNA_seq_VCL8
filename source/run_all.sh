ecd33f0f3faa2d5e5c265a792d36c498ba674ee5
Deason Yu <mr.d_style@hotmail.com>
2017-06-21 10:54:44 -0700

    kallisto time usage

--- source/run_all.sh
+++ source/run_all.sh
@@ -1,8 +1,14 @@
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
 
 ./get_barcode ../example_dataset/config.json
+
 ./error_correct_and_split ../example_dataset/config.json
+
+kstart = `date +'%Y-%m-%d %H:%M:%S'`
 ./compute_TCCs ../example_dataset/config.json
+kend = `date +'%Y-%m-%d %H:%M:%S'`
+echo "kallisto running time: "$((kend-kstart))"s"
+
 ./prep_TCC_matrix ../example_dataset/config.json
 
 endtime=`date +'%Y-%m-%d %H:%M:%S'`

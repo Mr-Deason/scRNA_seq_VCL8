18b58ebe16eecc2706048ca7a8d2aa6b1d3ffbbd
Deason Yu <mr.d_style@hotmail.com>
2017-06-21 14:21:24 -0400

    update

--- source/run_all.sh
+++ source/run_all.sh
@@ -4,10 +4,12 @@ starttime=`date +'%Y-%m-%d %H:%M:%S'`
 
 ./error_correct_and_split ../example_dataset/config.json
 
-kstart = `date +'%Y-%m-%d %H:%M:%S'`
+kstart=`date +'%Y-%m-%d %H:%M:%S'`
 ./compute_TCCs ../example_dataset/config.json
-kend = `date +'%Y-%m-%d %H:%M:%S'`
-echo "kallisto running time: "$((kend-kstart))"s"
+kend=`date +'%Y-%m-%d %H:%M:%S'`
+kstart_s=$(date --date="$kstart" +%s);
+kend_s=$(date --date="$kend" +%s);
+echo "kallisto running time: "$((kend_s-kstart_s))"s"
 
 ./prep_TCC_matrix ../example_dataset/config.json
 

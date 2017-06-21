3e8b9ac5b42f8b98373f1240c6923b907e12a94b
Deason Yu <mr.d_style@hotmail.com>
2017-06-21 02:24:09 -0700

    time

--- source/run_all.sh
+++ source/run_all.sh
@@ -1,4 +1,11 @@
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+
 ./get_barcode ../example_dataset/config.json
 ./error_correct_and_split ../example_dataset/config.json
 ./compute_TCCs ../example_dataset/config.json
 ./prep_TCC_matrix ../example_dataset/config.json
+
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s);
+end_seconds=$(date --date="$endtime" +%s);
+echo "running time: "$((end_seconds-start_seconds))"s"

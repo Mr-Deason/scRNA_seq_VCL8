cbb654a025e2399f99a03be606a63ca977f8a861
Deason Yu <mr.d_style@hotmail.com>
2017-07-26 08:59:18 -0400

    sh

--- source/run_hgmm.sh
+++ source/run_hgmm.sh
@@ -1,14 +1,14 @@
-~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_10k/config.json > ~/downloads/hgmm_fastqs/subset_10k/runinfo
-rm -rf ~/downloads/hgmm_fastqs/subset_10k/save_dir ~/downloads/hgmm_fastqs/subset_10k/output ~/downloads/hgmm_fastqs/subset_10k/TCC_output
+#~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_10k/config.json > ~/downloads/hgmm_fastqs/subset_10k/runinfo
+#rm -rf ~/downloads/hgmm_fastqs/subset_10k/save_dir ~/downloads/hgmm_fastqs/subset_10k/output ~/downloads/hgmm_fastqs/subset_10k/TCC_output
 
-~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_100k/config.json > ~/downloads/hgmm_fastqs/subset_100k/runinfo
-rm -rf ~/downloads/hgmm_fastqs/subset_100k/save_dir ~/downloads/hgmm_fastqs/subset_100k/output ~/downloads/hgmm_fastqs/subset_100k/TCC_output
+#~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_100k/config.json > ~/downloads/hgmm_fastqs/subset_100k/runinfo
+#rm -rf ~/downloads/hgmm_fastqs/subset_100k/save_dir ~/downloads/hgmm_fastqs/subset_100k/output ~/downloads/hgmm_fastqs/subset_100k/TCC_output
 
-~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_1m/config.json > ~/downloads/hgmm_fastqs/subset_1m/runinfo
-rm -rf ~/downloads/hgmm_fastqs/subset_1m/save_dir ~/downloads/hgmm_fastqs/subset_1m/output ~/downloads/hgmm_fastqs/subset_1m/TCC_output
+#~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_1m/config.json > ~/downloads/hgmm_fastqs/subset_1m/runinfo
+#rm -rf ~/downloads/hgmm_fastqs/subset_1m/save_dir ~/downloads/hgmm_fastqs/subset_1m/output ~/downloads/hgmm_fastqs/subset_1m/TCC_output
 
 ~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_5m/config.json > ~/downloads/hgmm_fastqs/subset_5m/runinfo
-rm -rf ~/downloads/hgmm_fastqs/subset_5m/save_dir ~/downloads/hgmm_fastqs/subset_5m/output ~/downloads/hgmm_fastqs/subset_5m/TCC_output
+#rm -rf ~/downloads/hgmm_fastqs/subset_5m/save_dir ~/downloads/hgmm_fastqs/subset_5m/output ~/downloads/hgmm_fastqs/subset_5m/TCC_output
 
-~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_10m/config.json > ~/downloads/hgmm_fastqs/subset_10m/runinfo
-rm -rf ~/downloads/hgmm_fastqs/subset_10m/save_dir ~/downloads/hgmm_fastqs/subset_10m/output ~/downloads/hgmm_fastqs/subset_10m/TCC_output
+#~/github/scRNA_seq_VCL8/source/run.sh ~/downloads/hgmm_fastqs/subset_10m/config.json > ~/downloads/hgmm_fastqs/subset_10m/runinfo
+#rm -rf ~/downloads/hgmm_fastqs/subset_10m/save_dir ~/downloads/hgmm_fastqs/subset_10m/output ~/downloads/hgmm_fastqs/subset_10m/TCC_output

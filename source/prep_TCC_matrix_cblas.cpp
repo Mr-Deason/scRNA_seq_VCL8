85304a593a5a52a0013c22a250224a32330e9fbc
Deason Yu <mr.d_style@hotmail.com>
2017-09-20 15:44:28 +0800

    update

---
+++ source/prep_TCC_matrix_cblas.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <set>
+#include <time.h>
+#include <omp.h>
+#include <boost/filesystem.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>  
+#include <zlib.h>
+#include <cblas.h>
+#include "kseq.h"
+
+using namespace std;
+namespace fs = boost::filesystem;
+namespace pt = boost::property_tree;
+
+int main(int argc, char* argv[])
+{
+
+	if(argc != 2)
+	{
+		cout << "arg cnt" << endl;
+		return -1;
+	}
+	if (!fs::is_regular_file(argv[1]))
+	{
+		cout << "json" << endl;
+		return -1;
+	}
+	pt::ptree root;
+	pt::read_json(argv[1], root);
+	
+	int D_MIN = root.get<int>("dmin");
+	int BARCODE_LENGTH = root.get<int>("BARCODE_LENGTH");
+	int NUM_THREADS = root.get<int>("NUM_THREADS");
+
+	string SOURCE_DIR = root.get<string>("SOURCE_DIR");
+	string SAVE_DIR = root.get<string>("SAVE_DIR");
+	string BASE_DIR = root.get<string>("BASE_DIR");
+	string OUTPUT_DIR = root.get<string>("OUTPUT_DIR");
+	string TCC_output = root.get<string>("kallisto.TCC_output");
+	string kallisto_binary = root.get<string>("kallisto.binary");
+	string kallisto_index = root.get<string>("kallisto.index");
+
+	vector<int> WINDOWS;
+	pt::ptree child_win = root.get_child("WINDOW");
+	BOOST_FOREACH(pt::ptree::value_type &v, child_win)
+		WINDOWS.push_back(v.second.get_value<int>());
+
+	string ecfile_dir = TCC_output + "matrix.ec";
+	string tsvfile_dir = TCC_output + "matrix.tsv";
+
+	cout << "Loading TCCs..." << endl;
+
+	FILE *fp;
+	fp = fopen(tsvfile_dir.c_str(), "rb");
+	int a, b, x;
+	vector<int> rows, cols, data;
+	while (fscanf(fp, "%d%d%d", &a, &b, &x) != EOF)
+	{
+		cols.push_back(a);
+		rows.push_back(b);
+		data.push_back(x);
+	}
+	fclose(fp);
+
+	unordered_map<int, int> map_rows;
+	vector<int> uni_rows(rows), uni_cols(cols);
+	sort(uni_rows.begin(), uni_rows.end());
+	uni_rows.erase(unique(uni_rows.begin(), uni_rows.end()), uni_rows.end());
+	for (int i = 0; i < uni_rows.size(); ++i)
+		map_rows[uni_rows[i]] = i;
+
+	int NUM_OF_CELLS = uni_rows.size();
+	cout << "NUM_OF_CELLS = " << NUM_OF_CELLS << endl;
+	double* rows_sum = new double[uni_rows.size()];
+	memset(rows_sum, 0, uni_rows.size() * sizeof(double));
+	for (int i = 0; i < rows.size(); ++i)
+		rows_sum[map_rows[rows[i]]] += data[i];
+	for (int i = 0; i < uni_cols.size(); ++i)
+		map_cols[uni_cols[i]] = i;
+
+	int NUM_OF_CELLS = uni_rows.size();
+	cout << "NUM_OF_CELLS = " << NUM_OF_CELLS << endl;
+	double** TCCmatrix = new double*[rows.size()];
+	for (int i = 0; i < uni_rows.size(); ++i)
+	{
+		TCCmatrix[i] = new double[uni_cols.size()];
+		memset(TCCmatrix[i], 0, uni_cols.size() * sizeof(double));
+	}
+	double* rows_sum = new double[uni_rows.size()];
+	memset(rows_sum, 0, uni_rows.size() * sizeof(double));
+	for (int i = 0; i < rows.size(); ++i)
+	{
+		TCCmatrix[map_rows[rows[i]]][map_cols[cols[i]]] = data[i];
+		rows_sum[map_rows[rows[i]]] += data[i];
+	}
+	for (int i = 0; i < rows.size(); ++i)
+	{
+		TCCmatrix[map_rows[rows[i]]][map_cols[cols[i]]] /= rows_sum[map_rows[rows[i]]];
+	}
+
+	cout << "Calculating pairwise L1 distances..." << endl;
+	int t0 = clock();
+	double** dist = new double*[NUM_OF_CELLS];
+	for (int i = 0; i < NUM_OF_CELLS; ++i)
+	{
+		dist[i] = new double[NUM_OF_CELLS];
+		memset(dist[i], 0, NUM_OF_CELLS * sizeof(double));
+	}
+
+
+
+	cout << uni_cols.size() << endl;
+	int cnt = 0;
+	int t0 = clock();
+	time_t tt0 = time(NULL);
+	
+	#pragma omp parallel for num_threads(8)
+	for (int i = 0; i < NUM_OF_CELLS; ++i)
+	{
+		for (int j = i+1; j < NUM_OF_CELLS; ++j)
+		{
+			for (int k = 0; k < uni_cols.size(); ++k)
+				dist[i][j] += fabs(TCCmatrix[i][k] - TCCmatrix[j][k]);
+			dist[j][i] = dist[i][j];
+		}
+	}
+	
+	cout << "time: " << (time(NULL) - tt0) << " s" << endl;
+	cout << "DONE" << endl;
+
+	return 0;
+}

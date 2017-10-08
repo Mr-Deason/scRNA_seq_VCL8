9aad52c93f4fade6cb90c8b24a909b9d0ccb52ac
Deason Yu <mr.d_style@hotmail.com>
2017-10-08 16:15:28 -0700

    upd

--- source/prep_TCC_matrix_eigen.cpp
+++ source/prep_TCC_matrix_eigen.cpp
@@ -149,10 +149,14 @@ int main(int argc, char* argv[])
 	double *vec_buff = new double[uni_cols.size()];
 	for (int i = 0; i < NUM_OF_CELLS; ++i)
 	{
+		cout << i << endl;
 		for (int j = i+1; j < NUM_OF_CELLS; ++j)
 		{
 			dist[i][j] = (TCCmat.row(i)-TCCmat.row(j)).cwiseAbs().sum();
-			assert (dist[j][i] == dist[i][j]);
+			if (dist[j][i] != dist[i][j])
+			{
+				cout << "error" << endl;
+			}
 		}
 	}
 

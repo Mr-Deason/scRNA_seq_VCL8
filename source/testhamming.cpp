6194f1b6497f932997f2518a933e8c4a9150f7ad
Deason Yu <mr.d_style@hotmail.com>
2017-10-23 20:30:09 -0700

    hamming

---
+++ source/testhamming.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "fasthamming.hpp"
+
+using namespace std;
+int main()
+{
+	nacgt<uint32_t> a = new nacgt<uint32_t>("ACGG", 4);
+	nacgt<uint32_t> b = new nacgt<uint32_t>("ACGN", 4);
+
+	hamming<uint32_t> hammingEval=hamming<uint32_t>(4);
+	cout << hammingEval.distance(a, b, 4) << endl;
+
+	return 0;
+}

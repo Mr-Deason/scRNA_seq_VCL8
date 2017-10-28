a182f77d28eea851af32ad23a08d7e3d6c609c6d
Deason Yu <mr.d_style@hotmail.com>
2017-10-28 04:22:56 -0700

    fix

--- source/error_correct_and_split.cpp
+++ source/error_correct_and_split.cpp
@@ -165,7 +165,7 @@ int main(int argc, char *argv[])
 	FILE *fp;
 	string bar_file = "barcodes.dat";
 	fp = fopen((SAVE_DIR + bar_file).c_str(), "rb");
-	while (fscanf(fp, "%" SCNu64, &read) != EOF)
+	while (fscanf(fp, "%" SCNu64 "", &read) != EOF)
 		barcodes.push_back(read);
 	fclose(fp);
 	string codes_file = "codewords.dat";

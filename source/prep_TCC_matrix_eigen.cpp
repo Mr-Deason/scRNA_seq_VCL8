56d804eb2f2ddddc3258819476df99c2b520a40f
Deason Yu <mr.d_style@hotmail.com>
2017-11-28 15:38:07 -0800

    eigen omp on

--- source/prep_TCC_matrix_eigen.cpp
+++ source/prep_TCC_matrix_eigen.cpp
@@ -121,7 +121,7 @@ int main(int argc, char* argv[])
 	{
 		const auto &row = TCCmat.row(i);
 		//#pragma omp parallel for shared(row)
-		//#pragma omp parallel for num_threads(NUM_THREADS)
+		#pragma omp parallel for num_threads(NUM_THREADS)
 		for (int j = i+1; j < NUM_OF_CELLS; ++j)
 		{
 			dist[i][j] = (row-TCCmat.row(j)).lpNorm<1>();

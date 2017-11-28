9d5fe3e2489fefb42e4a08d6bd46fb38b7ab0228
Deason Yu <mr.d_style@hotmail.com>
2017-11-27 22:07:03 -0800

    fix

--- source/testhamming.cpp
+++ source/testhamming.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	nacgt<uint32_t> *a = new nacgt<uint32_t>("ACGG", 4);
-	nacgt<uint32_t> *b = new nacgt<uint32_t>("ACGN", 4);
+	nacgt<uint32_t> *b = new nacgt<uint32_t>("ACGC", 4);
 
 	hamming<uint32_t> hammingEval=hamming<uint32_t>(4);
 	cout << hammingEval.distance(a, b, 4) << endl;

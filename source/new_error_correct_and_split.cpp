8c1debecd2f2739a5462ae4ab74bb8d57bb17a23
Deason Yu <mr.d_style@hotmail.com>
2017-12-08 21:39:33 -0800

    fix

--- source/new_error_correct_and_split.cpp
+++ source/new_error_correct_and_split.cpp
@@ -307,6 +307,7 @@ int main(int argc, char *argv[])
 	FILE *fp_umi_list = fopen((OUTPUT_DIR+umi_read_file).c_str(), "wb");
 	int flag = 0;
 	vector<string> output_fastqs;
+	char filename[40], fastq_file[100], fastq_gz_file[100], umi_file[100];
 	for (int i = 0; i < codewords.size(); ++i)
 	{
 		sprintf(filename, "cell_%04d_%s", i, decode(codewords[i], BARCODE_LENGTH).c_str());
